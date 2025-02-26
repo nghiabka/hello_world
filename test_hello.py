aaa0c28cb8be9ce75bf63eb9203f46b99cbadcfc
nghiadv <daonghiabk@gmail.com>
2025-02-26 23:14:37 +0700

    remove files

--- test_hello.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from hello import hello
-
-class TestHello(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(hello(), "Hello, World!")
-
-if __name__ == '__main__':
-    unittest.main()
